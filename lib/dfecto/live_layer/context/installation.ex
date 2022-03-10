182c2b71ef74aa8ec8871545545b9d150d811671
Joaquín Monteagudo Gómez <joaquin@doofinder.com>
2022-03-10 16:51:56 +0100

    Update installation context

--- lib/dfecto/live_layer/context/installation.ex
+++ lib/dfecto/live_layer/context/installation.ex
@@ -1,13 +1,12 @@
-defmodule Doomanager.LiveLayer.Installation do
+defmodule Dfecto.LiveLayer.Context.Installations do
   @moduledoc """
   Module for managing Installations
   """
   import Ecto.Query, warn: false
+  import Dfecto.Repo, only: [repo: 0]
 
-  alias Doomanager.Events
-  alias Doomanager.LiveLayer.Queries.Installation, as: InstallationQuery
-  alias Doomanager.LiveLayer.Schemas.Installation, as: InstallationSchema
-  alias Doomanager.Repo
+  alias Dfecto.Events
+  alias Dfecto.LiveLayer.Schemas.Installation, as: InstallationSchema
 
   @default_config %{
     "url_hash" => true
@@ -16,48 +15,54 @@ defmodule Doomanager.LiveLayer.Installation do
   @doc """
   Returns the list of installations.
   """
-  @spec list(pos_integer) :: [InstallationSchema.t()]
-  def list(account_id) do
+  @spec list(Ecto.Repo.t(), pos_integer) :: [InstallationSchema.t()]
+  def list(r \\ repo(), account_id) do
     account_id
     |> InstallationQuery.for_account()
-    |> Repo.all()
+    |> r.all()
   end
+  |> r.insert()
+
+with {:ok, installation} <- created_installation
 
   @doc """
   Gets a single installation.
   """
-  @spec get!(binary) :: InstallationSchema.t()
-  def get!(id), do: Repo.get!(InstallationSchema, id)
+  @spec get!(Ecto.Repo.t(), binary) :: InstallationSchema.t()
+  def get!(r \\ repo(), id), do: Repo.get!(InstallationSchema, id)
 
   @doc """
    Gets a single installation.
   """
-  @spec get(binary) :: InstallationSchema.t() | nil
+  @spec get(Ecto.Repo.t(), binary) :: InstallationSchema.t() | nil
   def get(id), do: Repo.get(InstallationSchema, id)
 
   @doc """
   Creates a installation.
   """
-  @spec create(map) ::
+  @spec create(map) :: {:ok, InstallationSchema.t()} | {:error, Ecto.Changeset.t() | any()}
+  def create(attrs \\ %{}), do: create(repo(), attrs)
+
+  @spec create(Ecto.Repo.t(), map) ::
           {:ok, InstallationSchema.t()} | {:error, Ecto.Changeset.t() | any()}
-  def create(attrs \\ %{}) do
+  def create(r, attrs) do
     attrs =
       if is_map(Map.get(attrs, :config)),
         do: Map.update(attrs, :config, %{}, &Map.merge(@default_config, &1)),
         else: attrs
 
-    Repo.transaction(fn ->
+    r.transaction(fn ->
       created_installation =
         %InstallationSchema{}
         |> InstallationSchema.changeset(attrs)
-        |> Repo.insert()
+        |> r.insert()
 
       with {:ok, installation} <- created_installation,
            :ok <- Events.trigger_event(installation, :created) do
         installation
       else
         {:error, error} ->
-          Repo.rollback(error)
+          r.rollback(error)
       end
     end)
   end
@@ -65,21 +70,21 @@ defmodule Doomanager.LiveLayer.Installation do
   @doc """
   Updates a installation.
   """
-  @spec update(InstallationSchema.t(), map) ::
+  @spec update(Ecto.Repo.t(), InstallationSchema.t(), map) ::
           {:ok, InstallationSchema.t()} | {:error, Ecto.Changeset.t() | any()}
-  def update(%InstallationSchema{} = installation, attrs) do
-    Repo.transaction(fn ->
+  def update(r \\ repo(), %InstallationSchema{} = installation, attrs) do
+    r.transaction(fn ->
       updated_installation =
         installation
         |> InstallationSchema.changeset(attrs)
-        |> Repo.update()
+        |> r.update()
 
       with {:ok, installation} <- updated_installation,
            :ok <- Events.trigger_event(installation, :updated) do
         installation
       else
         {:error, error} ->
-          Repo.rollback(error)
+          r.rollback(error)
       end
     end)
   end
@@ -87,16 +92,16 @@ defmodule Doomanager.LiveLayer.Installation do
   @doc """
   Deletes a installation.
   """
-  @spec delete(InstallationSchema.t()) ::
+  @spec delete(Ecto.Repo.t(), InstallationSchema.t()) ::
           {:ok, InstallationSchema.t()} | {:error, Ecto.Changeset.t() | any()}
-  def delete(%InstallationSchema{} = installation) do
-    Repo.transaction(fn ->
-      with {:ok, _} <- Repo.delete(installation),
+  def delete(r \\ repo(), %InstallationSchema{} = installation) do
+    r.transaction(fn ->
+      with {:ok, _} <- r.delete(installation),
            :ok <- Events.trigger_event(installation, :deleted) do
         installation
       else
         {:error, error} ->
-          Repo.rollback(error)
+          r.rollback(error)
       end
     end)
   end
